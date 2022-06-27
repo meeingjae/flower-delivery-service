35081cc25e0a3a2233c2cdc6558d79c423296eb0
Minjae Kim <minjae.kim@mytrum.com>
2022-06-27 10:41:45 +0900

    feat: Flower Data data preInsert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO D_FLOWER VALUES (1,'firstFlower',10000,'be happy');
+INSERT INTO D_FLOWER VALUES (2,'secondFlower',20000,'please be happy');
+INSERT INTO D_FLOWER VALUES (3,'thirdFlower',30000,'dont be sad');
+INSERT INTO D_FLOWER VALUES (4,'fourthFlower',40000,'must be happy');
+INSERT INTO D_FLOWER VALUES (5,'fifthFlower',50000,'happy birthday');
