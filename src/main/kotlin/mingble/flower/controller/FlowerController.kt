72b8397e1375cd0229cc2a9c81ce0b6c77f50547
Minjae Kim <minjae.kim@mytrum.com>
2022-06-27 11:10:38 +0900

    feat: Flower QueryDsl Impl

--- src/main/kotlin/mingble/flower/controller/FlowerController.kt
+++ src/main/kotlin/mingble/flower/controller/FlowerController.kt
@@ -2,6 +2,7 @@ package mingble.flower.controller
 
 import mingble.flower.entity.Flower
 import mingble.flower.repository.FlowerRepository
+import mingble.flower.repository.FlowerRepositoryCustom
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,10 +10,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/flower")
-class FlowerController (private val flowerRepository: FlowerRepository){
+class FlowerController (private val flowerRepository: FlowerRepository, private val flowerRepositoryCustom: FlowerRepositoryCustom){
 
     @GetMapping
     fun getAll() : List<Flower> {
         return flowerRepository.findAll()
+    }
+
+    @GetMapping("/dsl")
+    fun getAllByDsl() : List<Flower> {
+        return flowerRepositoryCustom.findAll()
     }
 }
